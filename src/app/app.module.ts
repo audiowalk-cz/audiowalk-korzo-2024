dd0a4300ed767e5e5e2135d4df785c0eabff783b
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-11-09 21:39:38 +0000

    feat: service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { ErrorHandler, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { ServiceWorkerModule } from "@angular/service-worker";
 import { MediaControlsController, StoryController } from "@audiowalk/sdk";
+import { config } from "src/config";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AppErrorHandler } from "./app.error-handler";
@@ -8,7 +10,16 @@ import { story } from "./data/story";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: config.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: "registerWhenStable:30000",
+    }),
+  ],
   providers: [
     { provide: ErrorHandler, useClass: AppErrorHandler },
     {
