21d5778a83f394a85439a9de214a19210dcaca18
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-11-09 16:31:42 +0000

    feat: update intro paes texts

--- src/app/story/pages/story-intro-page/story-intro-page.component.ts
+++ src/app/story/pages/story-intro-page/story-intro-page.component.ts
@@ -1,8 +1,5 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
-import { StoryController } from "@audiowalk/sdk";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { ChapterId, StoryState } from "src/app/data/story";
+import { UntilDestroy } from "@ngneat/until-destroy";
 
 @UntilDestroy()
 @Component({
@@ -10,28 +7,4 @@ import { ChapterId, StoryState } from "src/app/data/story";
   templateUrl: "./story-intro-page.component.html",
   styleUrl: "./story-intro-page.component.scss",
 })
-export class StoryIntroPageComponent {
-  isFromBeginning = true;
-
-  constructor(
-    private storyController: StoryController<ChapterId, StoryState>,
-    private router: Router,
-  ) {
-    storyController.storyState.pipe(untilDestroyed(this)).subscribe((state) => {
-      this.isFromBeginning = !state.storyStartedAt;
-    });
-  }
-
-  async startStory() {
-    await this.storyController.updateState((state) => ({
-      ...state,
-      storyStartedAt: state.storyStartedAt ?? new Date().toISOString(),
-    }));
-    this.router.navigate(["/story/walk"]);
-  }
-
-  async resetStory() {
-    await this.storyController.resetStory();
-    return this.startStory();
-  }
-}
+export class StoryIntroPageComponent {}
