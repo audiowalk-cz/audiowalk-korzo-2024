0b556a3796fdc13cf552c396253d61b72b19fd6f
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-11-11 15:07:11 +0000

    feat: update audio tracks

--- src/app/data/tracks.ts
+++ src/app/data/tracks.ts
@@ -87,7 +87,7 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   ambient_FF: {
     id: TrackId["ambient_FF"],
-    url: "assets/audio/INTERAKCE PARK ambient.mp3", // TODO: chybi track
+    url: "assets/audio/INTERAKCE FF ambient.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
@@ -141,7 +141,7 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   ambient_konicek: {
     id: TrackId["ambient_konicek"],
-    url: "assets/audio/INTERAKCE PARK ambient.mp3", // TODO: chybi track
+    url: "assets/audio/INTERAKCE KONICEK AMBIENT.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
@@ -279,7 +279,7 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   ambient_klementinum: {
     id: TrackId["ambient_klementinum"],
-    url: "assets/audio/INTERAKCE PARK ambient.mp3", // TODO: chybi track
+    url: "assets/audio/INTERAKCE KLEMENTINUM AMB.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
@@ -303,7 +303,7 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   ambient_betlemske: {
     id: TrackId["ambient_betlemske"],
-    url: "assets/audio/INTERAKCE PARK ambient.mp3", // TODO: chybi track
+    url: "assets/audio/INTERAKCE BETLEMSKE AMB.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
@@ -405,13 +405,13 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   interakce_3_3_a: {
     id: TrackId["interakce_3_3_a"],
-    url: "assets/audio/placeholder.mp3", // TODO: chybi track
+    url: "assets/audio/3.3.int.a.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
   interakce_3_3_b: {
     id: TrackId["interakce_3_3_b"],
-    url: "assets/audio/placeholder.mp3", // TODO: chybi track
+    url: "assets/audio/3.3.int.b.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
@@ -435,7 +435,7 @@ export const Tracks: { [key in TrackId]: { id: key } & TrackDefinition } = {
   },
   ambient_roh_narodni: {
     id: TrackId["ambient_roh_narodni"],
-    url: "assets/audio/INTERAKCE PARK ambient.mp3",
+    url: "assets/audio/INTERAKCE NARODKA AMB.mp3",
     type: "audio",
     mimeType: "audio/mpeg",
   },
